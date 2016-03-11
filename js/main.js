a5445329039b675a0a8fdc0a20ce2bc13538ebf1
Evan Lefebvre <lefe0177@algonquinlive.com>
2016-03-10 21:22:36 -0500

    Indentation

--- js/main.js
+++ js/main.js
@@ -1,48 +1,48 @@
 // Create your variable here, name it `insects`
 var insects = [
 {
-	name: 'Ant Nest Beetle',
-	img: 'ant-nest-beetle.jpg',
-	family: 'Carabidae',
-	size: [6, 20],
-	food: {
-		eatsInsects: true,
-		eatsDung: false,
-		eatsPlants: false,
-	}
+  name: 'Ant Nest Beetle',
+  img: 'ant-nest-beetle.jpg',
+  family: 'Carabidae',
+  size: [6, 20],
+  food: {
+    eatsInsects: true,
+    eatsDung: false,
+    eatsPlants: false,
+  }
 },
 {
-	name: 'Dung Beetle',
-	img: 'dung-beetle.jpg',
-	family: 'Scarabidae',
-	size: [20, 30],
-	food: {
-		eatsInsects: false,
-		eatsDung: true,
-		eatsPlants: false,
-	}
+  name: 'Dung Beetle',
+  img: 'dung-beetle.jpg',
+  family: 'Scarabidae',
+  size: [20, 30],
+  food: {
+    eatsInsects: false,
+    eatsDung: true,
+    eatsPlants: false,
+  }
 },
 {
-	name: 'Stag Beetle',
-	img: 'stag-beetle.jpg',
-	family: 'Lucanidae',
-	size: [5, 120],
-	food: {
-		eatsInsects: false,
-		eatsDung: false,
-		eatsPlants: true,
-	}
+  name: 'Stag Beetle',
+  img: 'stag-beetle.jpg',
+  family: 'Lucanidae',
+  size: [5, 120],
+  food: {
+    eatsInsects: false,
+    eatsDung: false,
+    eatsPlants: true,
+  }
 },
 {
-	name: 'Tortoise Beetle',
-	img: 'tortoise-beetle.jpg',
-	family: 'Cassidinae',
-	size: [1, 18],
-	food: {
-		eatsInsects: false,
-		eatsDung: false,
-		eatsPlants: true,
-	}
+  name: 'Tortoise Beetle',
+  img: 'tortoise-beetle.jpg',
+  family: 'Cassidinae',
+  size: [1, 18],
+  food: {
+    eatsInsects: false,
+    eatsDung: false,
+    eatsPlants: true,
+  }
 },
 ];
 
@@ -51,42 +51,42 @@ var insects = [
 // ##########################################################
 
 var writeInsects = function (bugs) {
-	document.write('<h1>Insects</h1>');
+  document.write('<h1>Insects</h1>');
 
-	document.write('<ol>');
+  document.write('<ol>');
 
-	bugs.forEach(function (bug) {
-		document.write('<li>');
-		document.write('<h2>' + bug.name + '</h2>');
-		document.write('<img src="images/' + bug.img + '" alt="">');
-		document.write('<dl>');
-		document.write('<dt>Family</dt>');
-		document.write('<dd><i>' + bug.family + '</i></dd>');
-		document.write('<dt>Size</dt>');
-		document.write('<dd>' + bug.size[0] + '–' + bug.size[1] + ' mm</dd>');
-		document.write('<dt>Eats</dt>');
+  bugs.forEach(function (bug) {
+    document.write('<li>');
+    document.write('<h2>' + bug.name + '</h2>');
+    document.write('<img src="images/' + bug.img + '" alt="">');
+    document.write('<dl>');
+    document.write('<dt>Family</dt>');
+    document.write('<dd><i>' + bug.family + '</i></dd>');
+    document.write('<dt>Size</dt>');
+    document.write('<dd>' + bug.size[0] + '–' + bug.size[1] + ' mm</dd>');
+    document.write('<dt>Eats</dt>');
 
-		document.write('<dd>');
+    document.write('<dd>');
 
-		if (bug.food.eatsInsects) {
-			document.write('Insects');
-		}
+    if (bug.food.eatsInsects) {
+      document.write('Insects');
+    }
 
-		if (bug.food.eatsDung) {
-			document.write('Dung');
-		}
+    if (bug.food.eatsDung) {
+      document.write('Dung');
+    }
 
-		if (bug.food.eatsPlants) {
-			document.write('Plants');
-		}
+    if (bug.food.eatsPlants) {
+      document.write('Plants');
+    }
 
-		document.write('</dd>');
+    document.write('</dd>');
 
-		document.write('</dl>');
-		document.write('</li>');
-	});
+    document.write('</dl>');
+    document.write('</li>');
+  });
 
-	document.write('</ol>');
+  document.write('</ol>');
 };
 
 writeInsects(insects);
